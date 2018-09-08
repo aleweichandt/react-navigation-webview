55e0ea6dbd0671597c394c962a9da588dfff85c9
Marcel Kalveram <marcel.kalveram@googlemail.com>
2018-09-08 17:36:56 +0200

    add Android back button functionality

--- src/WebViewDecorator.js
+++ src/WebViewDecorator.js
@@ -1,21 +1,95 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import asQuery from 'jquery-param';
+import React from "react";
+import PropTypes from "prop-types";
+import asQuery from "jquery-param";
 
-const withWebViewDecorator = (WebView) => {
-  const WebViewDecorator = ({ uri, navigation, ...navProps }) => {
-    const params = navigation.getParam('query') || undefined;
-    const url = params ? `${uri}?${asQuery(params)}` : uri;
-    const props = { ...navProps, navigation, source: { uri: url } };
-    return (
-      <WebView {...props} />
-    );
-  };
+const withWebViewDecorator = (WebView, BackHandler = null) => {
+  class WebViewDecorator extends React.Component {
+    _didFocusSubscription;
+    _willBlurSubscription;
+    _hasFocusSubscription;
+
+    state = {
+      canGoBack: false
+    };
+
+    constructor(props) {
+      super(props);
+      if (BackHandler) {
+        // this can fire before component has mounted ...
+        this._didFocusSubscription = this.props.navigation.addListener(
+          "didFocus",
+          // ...so this call also goes into componentDidMount
+          payload => this.addBackButtonListener()
+        );
+      }
+    }
+
+    componentDidMount() {
+      if (BackHandler) {
+        this.addBackButtonListener();
+        this._willBlurSubscription = this.props.navigation.addListener(
+          "willBlur",
+          payload =>
+            BackHandler.removeEventListener(
+              "hardwareBackPress",
+              this.onBackButtonPressAndroid
+            )
+        );
+      }
+    }
+
+    addBackButtonListener() {
+      // guard against adding this twice
+      if (!this.hasFocusSubscription) {
+        this.hasFocusSubscription = true;
+        BackHandler.addEventListener(
+          "hardwareBackPress",
+          this.onBackButtonPressAndroid
+        );
+      }
+    }
+
+    onNavigationStateChange(navState) {
+      if (BackHandler) {
+        this.setState({
+          canGoBack: navState.canGoBack
+        });
+      }
+    }
+
+    onBackButtonPressAndroid = () => {
+      if (this.state.canGoBack) {
+        this.webViewRef.goBack();
+        return true;
+      }
+      return false;
+    };
+
+    componentWillUnmount() {
+      this._didFocusSubscription && this._didFocusSubscription.remove();
+      this._willBlurSubscription && this._willBlurSubscription.remove();
+    }
+
+    render() {
+      const { uri, navigation, ...navProps } = this.props;
+      const params = navigation.getParam("query") || undefined;
+      const url = params ? `${uri}?${asQuery(params)}` : uri;
+      const props = { ...navProps, navigation, source: { uri: url } };
+      return (
+        <WebView
+          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+          ref={ref => (this.webViewRef = ref)}
+          {...props}
+        />
+      );
+    }
+  }
+
   WebViewDecorator.propTypes = {
     uri: PropTypes.string.isRequired,
     navigation: PropTypes.shape({
-      getParam: PropTypes.func.isRequired,
-    }).isRequired,
+      getParam: PropTypes.func.isRequired
+    }).isRequired
   };
   return WebViewDecorator;
 };
